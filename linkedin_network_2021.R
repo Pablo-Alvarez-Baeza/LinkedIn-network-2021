9410005c36eed074380e4f7b0e1d5e9edc7b157a
Pablo Alvarez Baeza <pablo.alvarez.baeza@gmail.com>
2021-12-07 15:27:50 +0100

    Update linkedin_network_2021.R

--- linkedin_network_2021.R
+++ linkedin_network_2021.R
@@ -4,14 +4,17 @@ p_load(tidyverse, janitor, lubridate, ggbeeswarm, ggtext, showtext, sysfonts)
 font_add("Fuzzy Bubbles", regular = "~~/Downloads/FuzzyBubbles-Bold.ttf")
 showtext_auto()
 
+theme_set(theme_minimal(base_family = "Lato"))
+
+
 
 raw_data <- read_csv("result.csv")
 
 df <- raw_data |>
   clean_names() |> 
   mutate(connection_since = as.Date(connection_since, "%d/%m/%y"),
-         month = month(connection_since),
-         network = factor(network)) |> 
+         network = factor(network),
+         connection = factor(connection)) |> 
   as.data.frame()
 
 df |> glimpse()
@@ -19,29 +22,37 @@ df |> glimpse()
 
 
 df |>
-  mutate(label_network = if_else(connection == 1, "", first_name),
-         connection = factor(connection)) |> 
+  mutate(label_network = if_else(connection == 1, "", first_name)) |> 
   ggplot(aes(1, connection_since, color = network, size = connection)) +
-  geom_beeswarm(alpha = .5,
+  geom_beeswarm(alpha = .8,
                 cex = 5) +
-  scale_size_manual(values = c(1, 4)) +
   geom_text(aes(label = label_network),
-            position=position_beeswarm(cex = 5) ,
+            position = position_beeswarm(cex = 5) ,
             size = 2,
             color = "black",
-            family="Fuzzy Bubbles") +
+            family = "Lato") +
   coord_flip() +
-  theme_minimal() +
-  scale_color_manual(values = c(
-    "w" = "grey25",
-    "ld" = "blue",
-    "dv" = "purple",
-    "other" = "grey85"
-  )) +
-  theme(
-    panel.grid = element_blank(),
-    legend.position = "none",
-    axis.text.y = element_blank()
-  ) +
+  scale_y_date(date_breaks = "1 month",
+               date_labels = "%b") +
   labs(x = NULL,
-       y = NULL)
+       y = NULL) +
+  scale_color_manual(values = c(
+    "w" = "#a6cee3",
+    "ld" = "#1f78b4",
+    "dv" = "#b2df8a",
+    "other" = "#BBBBBB"
+  )) +
+  scale_size_manual(values = c(2, 4),
+                    guide = 'none') +
+  theme(
+    plot.background = element_rect(fill = "grey98", color = "grey98"),
+    panel.background = element_rect(fill = "grey98", color = "grey98"),
+    legend.position = "top",
+    legend.justification = c(0, 1),
+    panel.grid = element_blank(),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(
+      family = "Lato",
+    )
+    ) 
+
